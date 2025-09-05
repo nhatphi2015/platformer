eb04957a1ba972d7f5988b10df95f127303c7a04
apple <apple@apples-MacBook-Pro.local>
2025-09-05 15:33:35 +0700

    i save my code

--- sprite.py
+++ sprite.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from setting import *
+import random
 vec = pg.math.Vector2
 
 class Spritesheet:
@@ -17,14 +18,33 @@ class Spritesheet:
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
-        self.game = game
         pg.sprite.Sprite.__init__(self)
-        self.image = self.game.spritesheet.get_image(692, 1667, 120, 132)
+        self.game = game
+        self.walking = False
+        self.jumping = False
+        self.current_frame = 0
+        self.last_update = 0
+        self.load_image()
+        self.image = self.standing_frame[0]
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2) 
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
+
+    def load_image(self):
+        self.standing_frame = [self.game.spritesheet.get_image(614, 1063, 120, 191),
+                               self.game.spritesheet.get_image(690, 406, 120, 201),]
+        for frame in self.standing_frame:
+            frame.set_colorkey(BLACK)
+        self.walk_frame_r = [ self.game.spritesheet.get_image(678, 860, 120, 201),
+                              self.game.spritesheet.get_image(692, 1458, 120, 207)]
+        self.walk_frame_l = []
+        for frame in self.walk_frame_r:
+            frame.set_colorkey(BLACK)
+            self.walk_frame_l.append(pg.transform.flip(frame, True, False))
+        self.jump_frame = self.image = self.game.spritesheet.get_image(416, 1660, 150, 181)
+        self.jump_frame.set_colorkey(BLACK)
 
     def jump(self):
         # jump only stand on the platform
@@ -35,6 +55,7 @@ class Player(pg.sprite.Sprite):
             self.vel.y = -PLAYER_JUMP
 
     def update(self):
+        self.animate()
         self.acc = vec(0, PLAYER_GRAVITY)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
@@ -46,20 +67,51 @@ class Player(pg.sprite.Sprite):
         self.acc += self.vel * PLAYER_FRICTION
         # equation of motion
         self.vel += self.acc
+        if abs(self.vel.x) < 0.1:
+            self.vel.x = 0
         self.pos += self.vel + 0.5 * self.acc
         # wrap around the sides of the screen
-        if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = WIDTH
+        if self.pos.x > WIDTH + self.rect.width / 2:
+            self.pos.x = 0 - self.rect.width / 2
+        if self.pos.x < 0 - self.rect.width / 2:
+            self.pos.x = WIDTH + self.rect.width / 2
 
         self.rect.midbottom = self.pos
 
+    def animate(self):
+        now = pg.time.get_ticks()
+        if self.vel.x != 0:
+            self.walking = True
+        else:
+            self.walking = False
+            # show walk animation
+        if self.walking:
+            if now - self.last_update > 200:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.walk_frame_l)
+                bottom = self.rect.bottom
+                if self.vel.x > 0:
+                    self.image = self.walk_frame_r[self.current_frame]
+                else:
+                    self.image = self.walk_frame_l[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
+        # show idle animation
+        if not self.jumping and not self.walking:
+            if now - self.last_update > 350:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.standing_frame)
+                bottom = self.rect.bottom
+                self.image = self.standing_frame[self.current_frame]
+                self.rect.bottom = bottom
+
 class Platform(pg.sprite.Sprite):
-    def __init__(self, x, y, w, h):
+    def __init__(self, game, x, y):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
-        self.image.fill(GREEN)
+        self.game = game
+        image = [self.game.spritesheet.get_image(0, 288, 380, 94), self.game.spritesheet.get_image(213, 1662, 201, 100)]
+        self.image = random.choice(image)
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
